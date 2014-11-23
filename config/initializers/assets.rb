a8d93badaba70df16c6da2ad75d93edcab10a01c
Ben Meier <benmeier42@gmail.com>
2014-11-23 03:02:02 +0200

    precompiled more assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,6 @@
 
+
+
 Rails.application.config.serve_static_assets = true
 Rails.application.config.assets.version = '1.0'
 
@@ -7,4 +9,5 @@ Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/javascripts"
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/stylesheets"
 
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)\z/
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
