0336864c7d928df468ae7b55113a59ef3cd20921
Ben Meier <benmeier42@gmail.com>
2015-02-04 00:31:05 +0200

    Make updated page only add new record if status has actually changed. otherwise simply update last timestamp

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -4,32 +4,31 @@ require 'open-uri'
 class UpdatesController < ApplicationController
 
   def latest
-    @latest_updates = Update.order(created_at: :desc).limit(20)
+    @latest_updates = Update.order(updated_at: :desc).limit(20)
   end
 
   def recheck
     uri = URI.parse('http://loadshedding.eskom.co.za/LoadShedding/GetStatus')
-    r = Net::HTTP.get(uri)
+    r = Net::HTTP.get(uri).to_i
 
-    case r.to_i
+    case r
     when 1
       active_stage = nil
-    when 2
-      active_stage = 1
-    when 3
-      active_stage = 2
-    when 4
-      active_stage = 3
-    when 5
-      active_stage = 4
+    when 2..5
+      active_stage = r - 1
     end
 
-    Update.create(
-      is_load_shedding_active: (not active_stage.nil?),
-      stage: (active_stage.nil? ? nil : active_stage)
-    )
+    last_update = Update.last
+    if last_update.nil? or last_update.stage != active_stage
+      Update.create(
+        is_load_shedding_active: (not active_stage.nil?),
+        stage: active_stage
+      )
+    else
+      last_update.touch
+    end
 
-    redirect_to latest_updates_path
+    redirect_to latest_updates_path, notice: "LoadShedding Status: #{last_update.stage} Updated: #{last_update.updated_at}"
   end
 
 end
