ec8818f0452fa2bfc0de615d75cb09a5f5c6cbed
Ben Meier <benmeier42@gmail.com>
2015-02-09 07:36:51 +0200

    enhanced syntax to not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def not_found
     respond_to do |format|
-      format.html { render 'error_pages/not_found', status: 404 }
+      format.html { render template: 'error_pages/not_found', status: 404 }
       format.all { render nothing: true, status: 404 }
     end
   end
